53579b9e042b71409d904451cd7fa885254af2cf
mashreb <achrefbensaid45@gmail.com>
2025-11-30 19:09:19 +0100

    refractor v2

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -104,22 +104,53 @@ private:
     void updatePeakFilter(const ChainSettings& chainSettings);
     using Coefficients = Filter::CoefficientsPtr;
     static void updateCoefficients(Coefficients& old, const Coefficients& replacements);
+    
+    template<int Index, typename ChainType, typename CoefficientType>
+    void update(ChainType& chain, const CoefficientType& coefficients)
+    {
+        updateCoefficients(chain.template get<Index>().coefficients, coefficients[Index]);
+        chain.template setBypassed<Index>(false);
+
+
+    }
+
     template<typename ChainType, typename CoefficientType>
-    void updateCutFilter(ChainType& leftLowCut,
+    void updateCutFilter(ChainType& chain,
         const CoefficientType& cutCoefficients,
     
         const Slope& lowCutSlope)
     {
      
-        leftLowCut.template setBypassed<0>(true);
-        leftLowCut.template setBypassed<1>(true);
-        leftLowCut.template setBypassed<2>(true);
-        leftLowCut.template setBypassed<3>(true);
+        chain.template setBypassed<0>(true);
+        chain.template setBypassed<1>(true);
+        chain.template setBypassed<2>(true);
+        chain.template setBypassed<3>(true);
 
 
         switch (lowCutSlope)
         {
+
+        case Slope_48:
+        {
+            update<3>(chain, cutCoefficients);
+
+        }
+        case Slope_36:
+        {
+            update<2>(chain, cutCoefficients);
+
+        }
+        case Slope_24:
+        {
+            update<1>(chain, cutCoefficients);
+
+        }
         case Slope_12:
+        {
+            update<0>(chain, cutCoefficients);
+
+        }
+       /* case Slope_12:
         {
             *leftLowCut.template get<0>().coefficients = *cutCoefficients[0];
             leftLowCut.template setBypassed<0>(false);
@@ -160,7 +191,7 @@ private:
             leftLowCut.template setBypassed<3>(false);
             break;
 
-        }
+        }*/
 
         }
 
