02fd340e600db5dad670b63fa9a8f21299996dfd
mashreb <achrefbensaid45@gmail.com>
2025-12-14 13:26:03 +0100

    Response curve sync LP HP filters

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -248,9 +248,7 @@ void updateCoefficients(Coefficients& old, const Coefficients& replacements)
 
 void EQ_PluginAudioProcessor::updateLowCutFilters(const ChainSettings& chainSettings)
 {
-    auto cutCoefficients = juce::dsp::FilterDesign<float>::designIIRHighpassHighOrderButterworthMethod(chainSettings.lowCutFreq,
-        getSampleRate(),
-        2 * (chainSettings.lowCutSlope + 1));
+    auto cutCoefficients = makeLowCutFilter(chainSettings, getSampleRate());
 
     auto& leftLowCut = leftChain.get<ChainPositions::LowCut>();
     auto& rightLowCut = rightChain.get<ChainPositions::LowCut>();
@@ -265,9 +263,8 @@ void EQ_PluginAudioProcessor::updateLowCutFilters(const ChainSettings& chainSett
 void EQ_PluginAudioProcessor::updateHighCutFilters(const ChainSettings& chainSettings)
 {
 
-    auto highCutCoefficients = juce::dsp::FilterDesign<float>::designIIRLowpassHighOrderButterworthMethod(chainSettings.highCutFreq,
-        getSampleRate(),
-        2 * (chainSettings.highCutSlope + 1));
+    auto highCutCoefficients = makeHighCutFilter(chainSettings, getSampleRate());
+
 
     auto& leftHighCut = leftChain.get<ChainPositions::HighCut>();
     auto& rightHighCut = rightChain.get<ChainPositions::HighCut>();
